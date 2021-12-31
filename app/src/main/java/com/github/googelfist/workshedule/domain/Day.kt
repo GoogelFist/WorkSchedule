4c7b8780fe28f067528e8b1bcb9c6db37616c70d
Nikita Gugel <googelfist@gmail.com>
2021-12-31 20:00:23 +0500

    add year field

--- app/src/main/java/com/github/googelfist/workshedule/domain/Day.kt
+++ app/src/main/java/com/github/googelfist/workshedule/domain/Day.kt
@@ -3,8 +3,9 @@ package com.github.googelfist.workshedule.domain
 data class Day(
     val value: Int,
     val month: Int,
-    val isActive: Boolean,
-    val isToday: Boolean,
-    val isWeekend: Boolean,
-    val isWork: Boolean
+    val year: Int,
+    var isActive: Boolean,
+    var isToday: Boolean,
+    var isWeekend: Boolean,
+    var isWork: Boolean
 )
