ec1207eb12ca0f3b75c2e383f455d95e0d1cdba6
Nikita Gugel <googelfist@gmail.com>
2022-01-04 17:08:28 +0500

    change AM

--- app/src/main/java/com/github/googelfist/workshedule/domain/models/DatePreference.kt
+++ app/src/main/java/com/github/googelfist/workshedule/domain/models/DatePreference.kt
@@ -1,7 +1,7 @@
 package com.github.googelfist.workshedule.domain.models
 
 data class DatePreference(
-    var date: String = DEFAULT_DATE
+    val date: String = DEFAULT_DATE
 )
 
 private const val DEFAULT_DATE = "Default date"
--- app/src/main/java/com/github/googelfist/workshedule/domain/models/SchedulePreference.kt
+++ app/src/main/java/com/github/googelfist/workshedule/domain/models/SchedulePreference.kt
@@ -1,7 +1,7 @@
 package com.github.googelfist.workshedule.domain.models
 
 data class SchedulePreference(
-    var schedule: String = DEFAULT_SCHEDULE
+    val schedule: String = DEFAULT_SCHEDULE
 )
 
 private const val DEFAULT_SCHEDULE = "Default schedule"
