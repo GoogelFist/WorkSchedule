663e70c9fe93c8991fd9d767f96bb0639dbf5c8d
Nikita Gugel <googelfist@gmail.com>
2022-01-20 04:15:49 +0500

    change locale to english, change pattern

--- app/src/main/java/com/github/googelfist/workschedule/data/schedulesgenerator/formatter/DateFormatterImpl.kt
+++ app/src/main/java/com/github/googelfist/workschedule/data/schedulesgenerator/formatter/DateFormatterImpl.kt
@@ -2,14 +2,15 @@ package com.github.googelfist.workschedule.data.schedulesgenerator.formatter
 
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 class DateFormatterImpl : DateFormatter {
 
     override fun format(date: LocalDate): String {
-        return DateTimeFormatter.ofPattern(FORMAT_PATTERN).format(date)
+        return DateTimeFormatter.ofPattern(FORMAT_PATTERN, Locale.ENGLISH).format(date)
     }
 
     companion object {
-        private const val FORMAT_PATTERN = "MMM yyyy"
+        private const val FORMAT_PATTERN = "MMMM yyyy"
     }
 }
